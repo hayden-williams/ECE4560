85d0f27e3e3d765e8dbb19338973fbc6f35f0249
hayden-williams <hwilliams123456789@gmail.com>
2017-10-18 18:37:08 -0400

    thetaZ was complex?

--- goStraight.py
+++ goStraight.py
@@ -12,7 +12,7 @@ import rospy
 import roslib
 from geometry_msgs.msg import Twist#, Pose
 from nav_msgs.msg import Odometry
-from cmath import sqrt
+from cmath import sqrt, 
 #from tf2_msgs.msg import TFMessage
 #import tf
 
@@ -62,7 +62,7 @@ class GoStraight():
 	def Orientation(self,data):
 		qz = data.pose.pose.orientation.z
 		qw = data.pose.pose.orientation.w
-		thetaZ = qz/sqrt(1-qw*qw)
+		thetaZ = qz/sqrt(1-(qw*qw))
 		#euler = self.tf.transformations.euler_from_quaternion(quaternion)
 		#yaw = euler[2]
 		rospy.loginfo("theta = %f"%(thetaZ))
