2178f99dbc74b56fa3d14df001a5c877e3faf238
hayden-williams <hwilliams123456789@gmail.com>
2017-10-29 21:06:46 -0400

    imported math for fabs

--- turninplace_userinput.py
+++ turninplace_userinput.py
@@ -13,6 +13,7 @@ import roslib
 from geometry_msgs.msg import Twist#, Pose
 from nav_msgs.msg import Odometry
 from cmath import *
+from math import fabs
 #from tf2_msgs.msg import TFMessage
 #import tf
 
