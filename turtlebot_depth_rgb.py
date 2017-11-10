42ee3d16c5d0dc003c478e36b9eec0bb3cda302d
hayden-williams <hwilliams123456789@gmail.com>
2017-11-10 16:48:11 -0500

    .

--- turtlebot_depth_rgb.py
+++ turtlebot_depth_rgb.py
@@ -15,7 +15,7 @@ class image_converter:
 
   def callback(self,data):
     try:
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(data, "mono16")
         cv.SaveImage("depth_camera_msg.jpg", cv_image)
         print "image saved!"
     except CvBridgeError, e:
