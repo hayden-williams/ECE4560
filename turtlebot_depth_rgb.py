87aed82d5fb8e3e2a3904e34cd4f82bfd39a05bd
hayden-williams <hwilliams123456789@gmail.com>
2017-11-10 17:45:39 -0500

    .

--- turtlebot_depth_rgb.py
+++ turtlebot_depth_rgb.py
@@ -8,33 +8,34 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 class image_converter:
+	
 	count = 0
 
-  def __init__(self):
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+	def __init__(self):
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
 
-  def callback(self,data):
-    try:
-        cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
-        cv2.imshow("depth_camera_msg.jpg", cv_image)
-        if count==0:
-        	cv2.waitKey(3)
-        	count=1
-        #cv2.waitKey(1)
-        print "image saved!"
+	def callback(self,data):
+		try:
+				cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
+				cv2.imshow("depth_camera_msg.jpg", cv_image)
+				if count==0:
+					cv2.waitKey(3)
+					count=1
+				#cv2.waitKey(1)
+				print "image saved!"
 
-    except CvBridgeError, e:
-      print e
+		except CvBridgeError, e:
+			print e
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
+	ic = image_converter()
+	rospy.init_node('image_converter', anonymous=True)
+	try:
 
-    rospy.spin()
-  except KeyboardInterrupt:
-    print "Shutting down"
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
 
 if __name__ == '__main__':
-    main(sys.argv)
+		main(sys.argv)
