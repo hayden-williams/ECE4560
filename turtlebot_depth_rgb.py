da422030290f03c205674cbd7df5c2b2427b03b9
hayden-williams <hwilliams123456789@gmail.com>
2017-11-10 16:31:01 -0500

    .

--- turtlebot_depth_rgb.py
+++ turtlebot_depth_rgb.py
@@ -10,23 +10,23 @@ from cv2 import *
 
 class image_converter:
 
-  def __init__(self):
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera/depth_registered/image_raw",Image,self.callback)
+	def __init__(self):
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/depth_registered/image_raw",Image,self.callback)
 
-  def callback(self,data):
-    try:
-        cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
-        cv2.imshow("depth_camera_msg.jpg", cv_image)
-        print "image saved!"
+	def callback(self,data):
+		try:
+				cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
+				cv2.imshow("depth_camera_msg.jpg", cv_image)
+				print "image saved!"
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print "Shutting down"
+	ic = image_converter()
+	rospy.init_node('image_converter', anonymous=True)
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
 
 if __name__ == '__main__':
-    main(sys.argv)
+		main(sys.argv)
