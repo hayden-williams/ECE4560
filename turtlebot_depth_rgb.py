441359769393cb211683badfb58f77d69e468c1a
hayden-williams <hwilliams123456789@gmail.com>
2017-11-10 16:47:23 -0500

    .

--- turtlebot_depth_rgb.py
+++ turtlebot_depth_rgb.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import roslib
-roslib.load_manifest('store_stereo_image')
 import sys 
 import rospy
-import cv
+import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -16,7 +15,7 @@ class image_converter:
 
   def callback(self,data):
     try:
-        cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
+        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         cv.SaveImage("depth_camera_msg.jpg", cv_image)
         print "image saved!"
     except CvBridgeError, e:
