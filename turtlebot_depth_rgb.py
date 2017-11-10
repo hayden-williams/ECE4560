1fd981c3004ea6a66f42dd1266525124867995de
hayden-williams <hwilliams123456789@gmail.com>
2017-11-10 18:06:51 -0500

    .

--- turtlebot_depth_rgb.py
+++ turtlebot_depth_rgb.py
@@ -29,7 +29,7 @@ class image_converter:
 		try:
 				depth_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
 				#depth_array = array(depth_image, dtype=float32)
-				cv2.normalize(depth_image, depth_image, 0, 1, cv2.NORM_MINMAX)
+				#cv2.normalize(depth_image, depth_image, 0, 1, cv2.NORM_MINMAX)
 				cv2.imshow("depth_camera_msg.jpg", depth_image)
 				cv2.waitKey(1)
 				#print "image saved!"
