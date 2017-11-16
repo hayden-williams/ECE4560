7610186a7f695ea0dcaa0e8314704bfc27696ea9
hayden-williams <hwilliams123456789@gmail.com>
2017-11-16 12:19:40 -0500

    neither work

---
+++ depth_sensor.py
@@ -0,0 +1,56 @@
+# Edgardo Marchand and Stephen Williams
+# Created: 16 Nov 2017
+
+#!/usr/bin/env python
+import roslib
+import sys 
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+
+	def __init__(self):
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+		self.image_sub2 = rospy.Subscriber("/camera/depth_registered/image_raw",Image,self.callback2)
+
+	def callback(self,data):
+		try:
+				cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+				#cv2.imshow("color_camera_msg.jpg", cv_image)
+				cv2.waitKey(1)
+				#print "image saved!"
+
+		except CvBridgeError, e:
+			print e
+
+	def callback2(self,data):
+		try:
+				depth_image = self.bridge.imgmsg_to_cv2(data, "mono16")
+
+
+				# print('[0,0] ' + str(float(depth_image)/1000) + ' millimetres.')
+				#depth_array = array(depth_image, dtype=float32)
+				#cv2.normalize(depth_image, depth_image, 0, 1, cv2.NORM_MINMAX)
+				cv2.imshow("depth_camera_msg.jpg", depth_image)
+				cv2.waitKey(1)
+				#print "image saved!"
+
+		except CvBridgeError, e:
+			print e
+
+def main(args):
+	ic = image_converter()
+	rospy.init_node('image_converter', anonymous=True)
+	try:
+
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
+
+if __name__ == '__main__':
+		main(sys.argv)
---
+++ one_color_object_detection.py
@@ -0,0 +1,42 @@
+# Stephen Williams and Edgardo Marchand
+# Created 16 Nov 2017
+
+# Homework 11
+#!/usr/bin/env python
+import roslib
+import sys 
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+
+	def __init__(self):
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+
+	def callback(self,data):
+		try:
+				cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+				cv2.imshow("color_camera_msg.jpg", cv_image)
+				cv2.waitKey(1)
+				#print "image saved!"
+
+		except CvBridgeError, e:
+			print e
+
+	
+def main(args):
+	ic = image_converter()
+	rospy.init_node('image_converter', anonymous=True)
+	try:
+
+		rospy.spin()
+	except KeyboardInterrupt:
+		print "Shutting down"
+
+if __name__ == '__main__':
+		main(sys.argv)
