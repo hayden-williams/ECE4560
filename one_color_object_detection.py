f69dd4151ce6d357d6cfa9a813af613ce159d78f
hayden-williams <hwilliams123456789@gmail.com>
2017-11-16 14:01:10 -0500

    .

--- one_color_object_detection.py
+++ one_color_object_detection.py
@@ -33,7 +33,7 @@ class image_converter:
 				cv2.imshow('res',res)
 
 				#cv2.imshow("color_camera_msg.jpg", cv_image)
-				cv2.waitKey(1)
+				cv2.waitKey(5)
 				#print "image saved!"
 
 		except CvBridgeError, e:
