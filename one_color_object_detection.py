77f97d3ad478ae22efaf5d59a7f805e8f9d199b3
hayden-williams <hwilliams123456789@gmail.com>
2017-11-16 13:37:30 -0500

    .

--- one_color_object_detection.py
+++ one_color_object_detection.py
@@ -10,9 +10,10 @@ import cv2
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np 
 
 class image_converter:
-
+	# detecting orange
 
 	def __init__(self):
 		self.bridge = CvBridge()
@@ -21,8 +22,15 @@ class image_converter:
 	def callback(self,data):
 		try:
 				cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-				cv2.imshow("color_camera_msg.jpg", cv_image)
-				cv2.waitKey(1)
+
+				lower_orange = np.array([240,100,0])
+    			upper_orange = np.array([255,200,200])
+    			mask = cv2.inRange(hsv, lower_orange, upper_orange)
+    			res = cv2.bitwise_and(cv_image,cv_image, mask= mask)
+    			cv2.imshow('res',res)
+
+				#cv2.imshow("color_camera_msg.jpg", cv_image)
+				cv2.waitKey(5)
 				#print "image saved!"
 
 		except CvBridgeError, e:
