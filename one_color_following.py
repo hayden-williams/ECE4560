1ee6720220fcf962a2815f9f2ed93c075f1b9814
hayden-williams <hwilliams123456789@gmail.com>
2017-11-30 18:11:05 -0500

    .

--- one_color_following.py
+++ one_color_following.py
@@ -14,13 +14,22 @@ import numpy as np
 
 class image_converter:
 	# detecting orange
+	setup =10
 
 	def __init__(self):
 		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,self.callback)
+		self.pub = rospy.Publisher('/cmd_vel_mux/input/navi',Twist)
+		self.msg = Twist()
+
+		r = rospy.Rate(10)
 
 	def callback(self,data):
 		try:
+
+				K = 1
+
+
 				cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
 				#hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
@@ -29,16 +38,49 @@ class image_converter:
 				upper_orange = np.array([120,130,255], dtype = "uint8")
 				mask = cv2.inRange(cv_image, lower_orange, upper_orange)
 				image = cv2.bitwise_and(cv_image,cv_image, mask= mask)
+
+
+				# Convert BGR to GReyscale
+				grey = cv2.cvtColor(self.output, cv2.COLOR_BGR2GRAY)
+
+
+				ret,thresh = cv2.threshold(grey,127,255,0)
+				contours,hierarchy = cv2.findContours(thresh, 1, 2)
+				cnt = contours[0]
+
+				M = cv2.moments(self.mask)
+				height, width, channels = image.shape #grey scale channel is 1, rgb is 3
+
+				if ( M['m00'] > 0):
+					cx = int(M['m10']/M['m00'])
+					cy = int(M['m01']/M['m00'])
+					centerOfObject = (int(cx),int(cy))
+					#cv2.circle(image,centerOfObject,10,(0,255,0),-1)
+
+					dx = cx - width/2 # +ve move right, -ve move left
+
+					if self.setup == 10:
+						move_cmd.linear.x = 0.0
+						move_cmd.angular.z = 0
+					else:
+						move_cmd.linear.x = 0.2
+						move_cmd.angular.z = K*dx
+				
+				self.cmd_vel.publish(self.move_cmd)
+
+				r.sleep()
+
+
 				#cv2.imshow('mask',mask)
 
 				#greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 				#ret,thresh = cv2.threshold(grey,127,255,0)
 
 
-				cv2.imshow('image',image)
+				#cv2.imshow('image',image)
 
 				#cv2.imshow("color_camera_msg.jpg", cv_image)
-				cv2.waitKey(3)
+				#cv2.waitKey(3)
 				#print "image saved!"
 
 		except CvBridgeError, e:
