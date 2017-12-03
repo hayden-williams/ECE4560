ca4e2f68f767a90191bc128c30edf4c30ff041fd
hayden-williams <hwilliams123456789@gmail.com>
2017-12-03 17:59:16 -0500

    .

--- one_color_following_with_depth.py
+++ one_color_following_with_depth.py
@@ -21,7 +21,9 @@ class image_converter:
 		rospy.init_node('image_converter', anonymous=True)
 		self.bridge = CvBridge()
 		self.image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,self.callback)
-		self.depth_sub = rospy.Subscriber("/camera/depth_registered/image_raw",Image,self.callback_depth)
+		self.depth_sub = rospy.Subscriber("/camera/depth/image",Image,self.callback)
+		#self.depth_sub = rospy.Subscriber("/camera/depth_registered/image_raw",Image,self.callback_depth)
+
 		self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
 		self.move_cmd = Twist()
 
